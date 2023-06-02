f5066c058b970604f7ae2ffafefdcc58a9f18a5e
Jayden Jang <newcephei@icloud.com>
2023-06-02 23:40:45 +0900

    Initial Commit

---
+++ Avocado/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Avocado
+//
+//  Created by Jayden Jang on 2023/06/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
