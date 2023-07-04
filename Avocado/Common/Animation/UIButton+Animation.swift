42cb0d517f2e9b9d409c2d8d01e40ca749b139a8
NUNU:D <hynjni7890@gmail.com>
2023-07-04 20:35:24 +0900

    [fix] folder issue 수정 [add] button animation 추가

---
+++ Avocado/Common/Animation/UIButton+Animation.swift
@@ -0,0 +1,33 @@
+//
+//  UIButton+Annimation.swift
+//  Avocado
+//
+//  Created by NUNU:D on 2023/07/04.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+    func keyboardMovement(from view: UIView, height: CGFloat, updateValue: CGFloat = 20) {
+        let updateHeight = height > 0 ? height : updateValue
+        let constraint = height > 0 ? 0 : updateValue
+        
+        UIView.animate(withDuration: 2.0) {
+            self.snp.updateConstraints {
+                $0.left.equalToSuperview().inset(constraint)
+            }
+            
+            self.layer.cornerRadius = constraint
+        }
+        
+        UIView.animate(withDuration: 0) {
+            self.snp.updateConstraints {
+                $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(updateHeight)
+            }
+        }
+        
+        view.layoutIfNeeded()
+    }
+}
+
