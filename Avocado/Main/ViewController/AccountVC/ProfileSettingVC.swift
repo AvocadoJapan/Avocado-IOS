d636d9aba76ae6a8fbf9d3d2efb79bfc05f6aa44
Jayden H Jang <Newcephei@icloud.com>
2023-07-04 20:06:57 +0900

    [FIX] 폴더구조 변경, SE대응 [ADD] 프로필화면 추가

---
+++ Avocado/Main/ViewController/AccountVC/ProfileSettingVC.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileSettingVC.swift
+//  Avocado
+//
+//  Created by Jayden Jang on 2023/07/04.
+//
+
+import Foundation
---
+++ Avocado/Main/ViewModel/ProfileSettingVM.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileSettingVM.swift
+//  Avocado
+//
+//  Created by Jayden Jang on 2023/07/04.
+//
+
+import Foundation
