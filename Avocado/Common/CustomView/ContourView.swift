ee14617f67f5dca3b36a9395c040ead8aac367a8
NUNU:D <hynjni7890@gmail.com>
2023-08-30 22:10:35 +0900

    [fix] 상품 디테일 화면 오토레이아웃 오류 수정

--- Avocado/Common/CustomView/ContourView.swift
+++ Avocado/Common/CustomView/ContourView.swift
@@ -11,11 +11,34 @@ import SnapKit
 
 final class ContourView: UIView {
     
+    // 구분선 화면
+    private lazy var lineView = UIView().then {
+        $0.backgroundColor = .systemGray6
+    }
+    
+    init(inset: UIEdgeInsets) {
+        super.init(frame: .zero)
+        setLayout()
+        setConstraint(inset: inset)
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .systemGray6
-        
-        self.snp.makeConstraints {
+        setLayout()
+        setConstraint()
+    }
+    
+    private func setLayout() {
+        addSubview(lineView)
+    }
+    
+    /**
+     * - description 오토레이아웃 설정 메서드로 파라미터 값을 받아 인셋을 정해줄 수 있도록 구성
+     * - parameter inset lineView의 inset값 기본값은 0
+     */
+    private func setConstraint(inset: UIEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)) {
+        lineView.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(inset)
             $0.height.equalTo(1)
         }
         
